91063f44fe664cc6072ad2009e4a6a3945de8c89
Dyson6235 <ghostpants1111@163.com>
2020-07-31 11:20:45 +0800

    solution for merge array

---
+++ Week_01/Solution_mergeArray.java
@@ -0,0 +1,8 @@
+import java.util.Arrays;
+
+public class Solution_mergeArray {
+    public void merge(int[] nums1, int m, int[] nums2, int n) {
+        System.arraycopy(nums2,0,nums1,m,n);
+        Arrays.sort(nums1);
+    }
+}
