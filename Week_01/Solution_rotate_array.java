aedfdcd3ebb3a7452d53697363472ab4880fb199
Dyson6235 <ghostpants1111@163.com>
2020-08-01 08:32:01 +0800

    solution for rotate array/reverse method

---
+++ Week_01/Solution_rotate_array.java
@@ -0,0 +1,18 @@
+public class Solution_rotate_array {
+    //事实上是先反转，把元素放入对应的区间（不考虑顺序），然后再以nums[k]为轴心旋转两次。
+    public void rotate(int[] nums, int k) {
+        k %= nums.length;
+        reverse(nums, 0, nums.length - 1);
+        reverse(nums, 0, k - 1);
+        reverse(nums, k, nums.length - 1);
+    }
+    public void reverse(int[] nums, int start, int end) {
+        while (start < end) {
+            int temp = nums[start];
+            nums[start] = nums[end];
+            nums[end] = temp;
+            start++;
+            end--;
+        }
+    }
+}
