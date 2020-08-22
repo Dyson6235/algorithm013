7d6a5551abf7d6dfa5d8f347e4eecaa03446df4d
Dyson6235 <ghostpants1111@163.com>
2020-08-22 11:15:30 +0800

    Solution for assign cookies

---
+++ Week_04/SolutionForAssignCookies.java
@@ -0,0 +1,19 @@
+package Solution;
+
+import java.util.Arrays;
+
+public class SolutionForAssignCookies {
+    public int findContentChildren(int[] grid, int[] size) {
+        if (grid == null || size == null) return 0;
+        Arrays.sort(grid);
+        Arrays.sort(size);
+        int gi = 0, si = 0;
+        while (gi < grid.length && si < size.length) {
+            if (grid[gi] <= size[si]) {
+                gi++;
+            }
+            si++;
+        }
+        return gi;
+    }
+}
